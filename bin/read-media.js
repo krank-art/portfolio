0b3df39d2c47216f80e7dbadf8696739f8612049
Krank <kranko.art@gmail.com>
2023-09-10 00:01:09 +0200

    Add script to read media in folder

--- bin/read-media.js
+++ bin/read-media.js
@@ -8,50 +8,68 @@ function toKebabCase(inputString) {
     .toLowerCase();
 }
 
-function readDir(dirPath, onFile = () => {}) {
+function readDir(dirPath, onFile = () => { }) {
   const fileNames = fs.readdirSync(dirPath);
   for (const fileName of fileNames) {
     const filePath = path.join(dirPath, fileName);
     const extension = path.extname(filePath);
     const stats = fs.statSync(filePath);
     const fileType = stats.isDirectory() ? 'dir' : 'file';
-    onFile({filePath, fileType, fileName, extension});
+    onFile({ filePath, fileType, fileName, extension });
   }
 }
 
-function readMedia(dirPath) {
+function writeObjectToFile(path, object) {
+  // depending on object size, it would be better to stream this
+  const content = JSON.stringify(object);
+  fs.writeFileSync(path, content, { encoding: 'utf-8' });
+}
+
+function readMediaInDir(dirPath) {
   const media = [];
-  readDir(dirPath, ({filePath, fileType, fileName, extension}) => {
+  readDir(dirPath, ({ filePath, fileType, fileName, extension }) => {
     if (fileType !== 'file') return;
     // For file names like 'Bear Conductor 2021-05-09 Release.png'.
     const fullNameRegex = /(.+)(\d{4}[-.]\d{2}[-.]\d{2}).*\.(.*)/g;
-    const match = fullNameRegex.exec(fileName);
-    if (match) {
-      const [, rawName, rawDate, extension] = match;
-      const mediaName = toKebabCase(rawName.trim());
-      const mediaDate = rawDate.replaceAll('.', '-');
-      media.push({
-        path: mediaName, 
-        date: mediaDate, 
-        fileName: fileName,
-        fileType: extension,
-        fileSize: 0,
-        widthInPx: 0,
-        heightInPx: 0,
-        aspectRatio: 0,
-        fileColors: [],
-        title: rawName.trim(),
-        description: "",
-        tags: [],
-        palette: [],
-      });
-      return;
-    }
-    //console.log(fileName);
+    const simpleNameRegex = /(.+)()\.(.*)/g;
+    const match = fullNameRegex.exec(fileName) ?? simpleNameRegex.exec(fileName);
+    const [, rawName, rawDate, fileExtension] = match;
+    const mediaName = toKebabCase(rawName.trim());
+    const mediaDate = rawDate.replaceAll('.', '-');
+    media.push({
+      path: mediaName,
+      date: mediaDate,
+      fileName: fileName,
+      fileType: fileExtension,
+      // TODO: Read meta data from image with 'sharp'
+      //fileSize: 0,
+      //widthInPx: 0,
+      //heightInPx: 0,
+      //aspectRatio: 0,
+      //imageColors: [],
+      title: rawName.trim(),
+      description: "",
+      tags: [],
+      palette: [],
+    });
   });
   return media;
 }
 
+function readMedia(dirPath) {
+  if (!dirPath) {
+    console.error(`No directory for media files specified. Aborting. `);
+    return;
+  }
+  if (!fs.existsSync(dirPath)) {
+    console.log(`Directory '${dirPath}' does not exist. Aborting. `);
+    return;
+  }
+  const filePath = path.resolve(dirPath);
+  const media = readMediaInDir(filePath);
+  const targetFile = path.resolve(process.cwd(), 'media.json');
+  writeObjectToFile(targetFile, media);
+}
+
 const [, , rawPath] = process.argv;
-const filePath = path.resolve(rawPath);
-console.log(readMedia(filePath));
+readMedia(rawPath);
