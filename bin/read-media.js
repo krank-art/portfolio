60e54c64683d4fc0ca5c20a4c831dea3434919e0
Krank <kranko.art@gmail.com>
2023-09-12 11:31:08 +0200

    Change 'read-media' to also get image metadata

--- bin/read-media.js
+++ bin/read-media.js
@@ -1,8 +1,37 @@
 import fs from 'fs';
 import path from 'path';
+import sharp from 'sharp';
 import { getVibrantColorsInImage } from '../lib/image.js';
 import { toKebabCase } from '../lib/string.js';
 import { writeObjectToFile } from '../lib/filesystem.js';
+
+async function processMediaFile({ filePath, fileType, fileName, extension, fileSize }) {
+  console.log(filePath);
+  if (fileType !== 'file') return;
+  // For file names like 'Bear Conductor 2021-05-09 Release.png'.
+  const fullNameRegex = /(.+)(\d{4}[-.]\d{2}[-.]\d{2}).*\.(.*)/g;
+  const simpleNameRegex = /(.+)()\.(.*)/g;
+  const match = fullNameRegex.exec(fileName) ?? simpleNameRegex.exec(fileName);
+  const [, rawName, rawDate, fileExtension] = match;
+  const mediaName = toKebabCase(rawName.trim());
+  const mediaDate = rawDate.replaceAll('.', '-');
+  const metadata = await sharp(filePath).metadata();
+  return Promise.resolve({
+    path: mediaName,
+    date: mediaDate,
+    fileName: fileName,
+    fileType: fileExtension,
+    fileSize: fileSize,
+    width: metadata.width,
+    height: metadata.height,
+    aspectRatio: metadata.width / metadata.height,
+    vibrantColors: await getVibrantColorsInImage(filePath),
+    title: rawName.trim(),
+    description: "",
+    tags: [],
+    palette: [],
+  });
+}
 
 async function readMediaItem(filePath) {
   const stats = await fs.promises.stat(filePath);
@@ -11,7 +40,8 @@ async function readMediaItem(filePath) {
   const fileName = path.basename(filePath);
   const extension = path.extname(filePath);
   const fileType = stats.isDirectory() ? 'dir' : 'file';
-  return Promise.resolve({ filePath, fileType, fileName, extension });
+  const fileSize = stats.size;
+  return Promise.resolve({ filePath, fileType, fileName, extension, fileSize });
 }
 
 async function readMediaInDir(dirPath) {
@@ -19,34 +49,7 @@ async function readMediaInDir(dirPath) {
   const files = await fs.promises.readdir(dirPath);
   for (const file of files) {
     const filePath = path.resolve(dirPath, file);
-    const mediaItem = await readMediaItem(filePath).then(
-      async ({ filePath, fileType, fileName, extension }) => {
-        console.log(filePath);
-        if (fileType !== 'file') return;
-        // For file names like 'Bear Conductor 2021-05-09 Release.png'.
-        const fullNameRegex = /(.+)(\d{4}[-.]\d{2}[-.]\d{2}).*\.(.*)/g;
-        const simpleNameRegex = /(.+)()\.(.*)/g;
-        const match = fullNameRegex.exec(fileName) ?? simpleNameRegex.exec(fileName);
-        const [, rawName, rawDate, fileExtension] = match;
-        const mediaName = toKebabCase(rawName.trim());
-        const mediaDate = rawDate.replaceAll('.', '-');
-        return Promise.resolve({
-          path: mediaName,
-          date: mediaDate,
-          fileName: fileName,
-          fileType: fileExtension,
-          // TODO: Read meta data from image with 'sharp'
-          //fileSize: 0,
-          //widthInPx: 0,
-          //heightInPx: 0,
-          aspectRatio: 0,
-          vibrantColors: await getVibrantColorsInImage(filePath),
-          title: rawName.trim(),
-          description: "",
-          tags: [],
-          palette: [],
-        });
-      });
+    const mediaItem = await readMediaItem(filePath).then(processMediaFile);
     media.push(mediaItem);
   }
   return media;
