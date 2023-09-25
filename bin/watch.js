c2ceff9d2a89c0a37282bea94b4470e795f8843f
Krank <kranko.art@gmail.com>
2023-09-25 14:29:08 +0200

    Add file watcher

--- bin/watch.js
+++ bin/watch.js
@@ -1,17 +1,28 @@
-const fs = require('fs');
+import fs from 'fs';
+import { pathing } from './cli.js';
+import { debounce } from '../lib/async.js';
 
-const filePath = 'path/to/watched/file-or-directory';
+function watchDir({ dirPath, onChange = () => { }, onError = () => { } }) {
+  const watcher = fs.watch(dirPath, (eventType, filename) => {
+    if (eventType === 'change') {
+      onChange(eventType, filename);
+    }
+  });
+  watcher.on('error', (error) => {
+    console.error(`Watcher error: ${error}`);
+    onError(error);
+  });
+  return watcher;
+}
 
-const watcher = fs.watch(filePath, (eventType, filename) => {
-  if (eventType === 'change') {
-    console.log(`File ${filename} has changed.`);
-    // Perform actions here when the file changes.
-  }
+const onFileChange = (eventType, fileName) => { 
+  console.log(eventType, `File ${fileName} has changed.`);
+};
+
+watchDir({
+  dirPath: pathing.pages,
+  // For some reason, when changing files with VS Code on Windows, there are 3-4 change events fired at once.
+  // As comparison, when changing a file with notepad.exe, only one event gets fired.
+  // So we debounce the callback for the event to only handle actual changes.
+  onChange: debounce(onFileChange, 50),
 });
-
-watcher.on('error', (error) => {
-  console.error(`Watcher error: ${error}`);
-});
-
-// To stop watching the file, you can call watcher.close() when needed.
-// watcher.close();
