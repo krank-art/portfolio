84e6aaed5cbb7cc0fd5e70e3254f8778b344ed65
Krank <kranko.art@gmail.com>
2023-09-25 18:24:10 +0200

    Add cli commands to watch files

--- bin/cli.js
+++ bin/cli.js
@@ -4,10 +4,13 @@ import { deleteDirRecursive, parseJsonFile } from '../lib/filesystem.js';
 import readMedia from './read-media.js';
 import buildHtml from './build-html.js';
 import buildAssets from './build-assets.js';
+import FileWatcher from '../lib/filewatcher.js';
+import { Color } from '../lib/terminal.js';
 
 export const pathing = Object.freeze({
   dist: path.resolve('dist'),
   pages: path.resolve("pages"),
+  assetsEntry: path.resolve("lib/main.js"),
   artData: path.resolve('data/media-art.json'),
   artImport: path.resolve("static/art"),
   artProcessed: path.resolve("dist/media"),
@@ -52,6 +55,53 @@ export async function handleCommand(command, ...args) {
         dirPath: pathing.artImport,
         outputFileName: "media-art",
         skipUnchanged: true,
+      });
+      break;
+    case "watch":
+      await handleCommand("watch:assets");
+      await handleCommand("watch:html");
+      await handleCommand("watch:art");
+      break;
+    case "watch:art":
+      console.log("Press Ctrl + C to abort watcher. ");
+      new FileWatcher({
+        target: [
+          pathing.artData,
+          pathing.artImport,
+        ],
+        recursive: true,
+        onChange: async (eventType, fileName) => {
+          console.log(Color.Blue + `'${eventType}' on file '${fileName}'. Rebuilding art files. `
+            + Color.Gray + `(${new Date()})` + Color.Reset);
+          await handleCommand("build:art");
+        },
+      });
+      break;
+    case "watch:html":
+      console.log("Press Ctrl + C to abort watcher. ");
+      new FileWatcher({
+        target: [
+          pathing.pages,
+          pathing.artData,
+        ],
+        recursive: true,
+        onChange: async (eventType, fileName) => {
+          console.log(Color.Orange + `'${eventType}' on file '${fileName}'. Rebuilding HTML. `
+            + Color.Gray + `(${new Date()})` + Color.Reset);
+          await handleCommand("build:html");
+        },
+      });
+      break;
+    case "watch:assets":
+      console.log("Press Ctrl + C to abort watcher. ");
+      new FileWatcher({
+        target: pathing.assetsEntry,
+        recursive: true,
+        onChange: async (eventType, fileName) => {
+          console.log(Color.Red + `'${eventType}' on file '${fileName}'. Rebuilding Assets. `
+            + Color.Gray + `(${new Date()})` + Color.Reset);
+          await handleCommand("build:assets");
+        },
       });
       break;
     default:
