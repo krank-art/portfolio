1a3cd371061bb965c3f2febb1dad32e9e0bcec8a
Krank <kranko.art@gmail.com>
2023-10-02 18:16:22 +0200

    Extract template rendering to TemplateWriter

--- bin/build-html.js
+++ bin/build-html.js
@@ -2,31 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import { ensureDirExists } from '../lib/filesystem.js';
 import config from '../config/config.dev.js';
-import TemplateEngine from '../lib/template-engine.js';
-
-function compileSfcDir(input, output, data, subpath = []) {
-  const files = fs.readdirSync(input);
-  for (const file of files) {
-    const filePath = path.join(input, file);
-    const outputName = path.basename(file, path.extname(file)) + ".html";
-    const outputPath = path.join(output, ...subpath);
-    const outputFile = path.join(outputPath, outputName);
-    const stats = fs.statSync(filePath);
-    if (stats.isDirectory()) {
-      compileSfcDir(filePath, output, data, [...subpath, file])
-      continue;
-    }
-    if (path.extname(filePath) !== ".hbs")
-      continue;
-    ensureDirExists(outputPath + path.sep);
-    TemplateEngine.compileSfc(filePath, outputPath, data, subpath.length);
-  }
-  //compile('pages/index.hbs', 'dist/template.html', 'layouts/default.hbs', data);
-  //renderSfc('pages/index.hbs', data);
-}
+import TemplateWriter from '../lib/template-writer.js';
 
 export default function buildHtml({ inputDir, outputDir, data }) {
-  compileSfcDir(inputDir, outputDir, {
+  TemplateWriter.compileSfcDir(inputDir, outputDir, {
     ...config,
     ...data,
   });
