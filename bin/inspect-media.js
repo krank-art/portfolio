88a7ca2559fe87b8ca3feec613d29ea0ab4a2f88
Krank <kranko.art@gmail.com>
2023-10-04 18:21:52 +0200

    Change 'inspect-media' so log table is not too large

--- bin/inspect-media.js
+++ bin/inspect-media.js
@@ -1,4 +1,4 @@
-import { formatTable, simplifyBytes } from "../lib/string.js";
+import { formatTable, simplifyBytes, truncateEllipsis } from "../lib/string.js";
 
 export function inspectMedia(media) {
   const lines = media.map(piece => {
@@ -10,9 +10,17 @@ export function inspectMedia(media) {
 
 
 export function inspectMediaTable(media) {
+  const fileNameMaxLength = 48;
   const lines = media.map(piece => {
-    const { fileNamePublic, fileNameInternal, fileSize, width, height, aspectRatio } = piece;
-    return [fileNameInternal, fileNamePublic, simplifyBytes(fileSize), width + " \u00d7 " + height, aspectRatio];
+    const { fileNamePublic, fileNameInternal, fileSize, width, height, aspectRatio, orientation } = piece;
+    return [
+      truncateEllipsis(fileNameInternal, fileNameMaxLength), 
+      truncateEllipsis(fileNamePublic, fileNameMaxLength), 
+      simplifyBytes(fileSize), 
+      width + " \u00d7 " + height, 
+      aspectRatio,
+      orientation,
+    ];
   });
   return formatTable(lines, 2);
 }
