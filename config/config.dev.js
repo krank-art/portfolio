2e4473ae13ce62e597a06904316ce9552239fa46
Krank <kranko.art@gmail.com>
2023-10-26 17:03:13 +0200

    Add config for error page

--- config/config.dev.js
+++ config/config.dev.js
@@ -7,4 +7,7 @@ export default {
   siteThumbnail: "art/960p/bear-tired_2022-10-25.png",
   siteThumbnailAlt: "Krank draws furry art, among other things. ",
   siteColor: "#3c543c",
+  errorThumbnail: "art/960p/bear-annoyed-stupid_2023-10-10.png",
+  errorThumbnailAlt: "The requested page could not be loaded. ",
+  errorColor: "#554637",
 }
