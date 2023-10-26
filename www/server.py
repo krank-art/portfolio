012f0ecdee1da76cfc9066cac61541649a7fc023
Krank <kranko.art@gmail.com>
2023-10-26 21:21:50 +0200

    Add aborting and 404 to python dev server

--- www/server.py
+++ www/server.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import os
+import sys
+import signal
 from http.server import HTTPServer
 from http.server import SimpleHTTPRequestHandler
 
-# IP has to be '0.0.0.0', so we can also reach it with over devices in the same LAN network.
+# IP has to be '0.0.0.0', so we can also reach it with other devices in the same LAN network.
 IP = '0.0.0.0' 
 PORT = 8000 
-DIRECTORY = ".." + os.sep + "dist"
+DIRECTORY = ".." + os.sep + ".." + os.sep + "dist"
 
 web_dir_raw = os.path.join(os.path.dirname(__file__), DIRECTORY)
 web_dir = os.path.abspath(web_dir_raw)
@@ -14,15 +16,21 @@ web_dir = os.path.abspath(web_dir_raw)
 class MyHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, directory=web_dir)
-    def translate_path(self, path):
-        # Omit .html extension for paths
-        if path != "/" and not "." in path:
-            path += ".html"
-        return super().translate_path(path)
     def do_GET(self):
-        super().do_GET()
+        if self.path != "/" and not "." in self.path:
+            self.path += ".html"
+        absolutePath = self.translate_path(self.path)
+        if not os.path.exists(absolutePath):
+            self.path = "404.html"
+        SimpleHTTPRequestHandler.do_GET(self)
+
+def stopServer(server: HTTPServer):
+    print(f'Stopping HTTP on {IP}:{PORT}. ')
+    server.shutdown()
+    sys.exit(0)
 
 if __name__ == '__main__':
     print(f'Serving HTTP on {IP}:{PORT} ...')
     httpd = HTTPServer((IP, PORT), MyHandler)
+    signal.signal(signal.SIGINT, lambda signum, frame: stopServer(httpd))
     httpd.serve_forever()
