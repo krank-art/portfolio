6c37923896597a93ba9ba8dcb9280b6796f7eac7
Krank <kranko.art@gmail.com>
2023-10-31 18:54:34 +0100

    Change rollup config to copy blog files to dist

--- rollup.config.js
+++ rollup.config.js
@@ -11,6 +11,7 @@ export default {
     copy({
       targets: [
         { src: 'static/favicon/*', dest: 'dist' },
+        { src: 'static/blog/*', dest: 'dist/media/blog' },
       ],
     }),
     sass({
