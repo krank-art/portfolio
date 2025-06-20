e00f54337951a6ec2d8fd483ebce5841e62378b1
Krank <kranko.art@gmail.com>
2025-06-21 01:03:45 +0200

    Fix database creation script

--- www/php/public/admin/create-comments-table.php
+++ www/php/public/admin/create-comments-table.php
@@ -11,17 +11,22 @@ require __DIR__ . '/../../database.php';
 
 $tableName = $config['comments_table'];
 
+// The HASH encodes values from 0 to 2^64 - 1. If we want to encode this with base64, we get 2^64 = 64^x.
+// Then if we solve it, we get x = 32/3 (10.667) which is close enough for 11.
+
 try {
     $pdo->exec("
         CREATE TABLE IF NOT EXISTS $tableName (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            approved TIMESTAMP DEFAULT NULL,
-            target VARCHAR(255) NOT NULL,
-            imagePath VARCHAR(255) NOT NULL,
-            username VARCHAR(100) NOT NULL,
-            website VARCHAR(255),
-            hash VARCHAR(11) NOT NULL UNIQUE
+            id           INT          AUTO_INCREMENT PRIMARY KEY,
+            created      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            approved     TIMESTAMP    DEFAULT NULL,
+            target       VARCHAR(255) NOT NULL,
+            imagePath    VARCHAR(255) NOT NULL,
+            historyPath  VARCHAR(255) NOT NULL,
+            username     VARCHAR(100) NOT NULL,
+            website      VARCHAR(255) DEFAULT NULL,
+            hash         CHAR(11)     NOT NULL UNIQUE,
+            submissionId CHAR(10)     NOT NULL,
         );
     ");
     echo "✅ Table '$tableName' created successfully (if already exists, nothing happened).";
