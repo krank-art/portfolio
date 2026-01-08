5fb832b531d61b9360715aec54ce9f720d1e4725
Krank <kranko.art@gmail.com>
2026-01-08 14:41:15 +0100

    Update site metadata to use different image

--- config/config.dev.js
+++ config/config.dev.js
@@ -6,10 +6,10 @@ export default {
   titleTemplate: "#{title} \u2013 Krank",
   siteName: "krank.love",
   siteTwitter: "@krankomat96",
-  siteThumbnail: "art/960p/bear-tired_2022-10-25.png",
+  siteThumbnail: "art/960p/bear-town_2023-12-29.png",
   siteThumbnailAlt: "Krank draws furry art, among other things. ",
-  siteColor: "#3c543c",
+  siteColor: "#8e7040",
   errorThumbnail: "art/960p/bear-annoyed-stupid_2023-10-10.png",
   errorThumbnailAlt: "The requested page could not be loaded. ",
-  errorColor: "#554637",
+  errorColor: "#aa5b32",
 }
